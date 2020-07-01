ae16c48e154345a178ea2bf5fad88feab83adf7d
unknown <qudtls1717@gmail.com>
2020-07-01 20:12:45 +0900

    20200701 20:12

--- src/api/Greetings/sayHello/sayHello.js
+++ src/api/Greetings/sayHello/sayHello.js
@@ -1,5 +1,10 @@
+import { prisma } from "../../../../generated/prisma-client";
+
 export default {
   Query: {
-    sayHello: () => "Hellow",
+    sayHello: async () => {
+      console.log(await prisma.users()); // prisma api에있는 필드 실행
+      return "Hellow";
+    },
   },
 };
